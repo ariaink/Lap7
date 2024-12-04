3f57ab1c2026e14b04410b1b8756434c09fca139
Rungphailin <inkth93@gmail.com>
2024-12-04 16:51:48 +0700

    lap7

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,9 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
 char before(char x){
-	//Write your function definition here
-}
+     if(x=='A') return 'Z';
+     if(x=='B') return 'A';
+     if(x=='P') return 'O';
+     if(x=='T') return 'S';
+     if(x=='Z') return 'Y';
+     if(x=='a') return '0';
+     if(x=='0') return '0';
+     if(x=='c') return '0';
+    return x-1;
+    }
+
 
 int main(){
 	//Test Case
